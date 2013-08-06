111050e70d3d8a40978d696929eba0c7d63067ed
Lou Scoras <louis.j.scoras@gmail.com>
2013-08-05 21:08:50 -0400

    Pull out CLI module

---
+++ Roller/CLI.hs
@@ -0,0 +1,23 @@
+module Roller.CLI (
+  CLI(..)
+, withOpts
+) where
+
+import Options.Applicative hiding (Const)
+import Control.Applicative
+
+type CLI a = Bool -> Int -> [String] -> a
+
+withOpts :: CLI a -> IO a
+withOpts f = execParser . info (helper <*> handleOpts) $ infoMod
+  where
+  handleOpts =
+    f <$> switch ( long "verbose"
+                <> short 'v'
+                <> help "List out each roll")
+      <*> option ( long "nrolls"
+                <> value 1
+                <> short 'n'
+                <> help "Number of times to roll the expression")
+      <*> arguments1 str (metavar "EXPR.." <> help "Dice expressions")
+  infoMod = fullDesc <> progDesc "Roll some dice"
--- Roller/Core.hs
+++ Roller/Core.hs
@@ -1,18 +1,13 @@
-module Roller.Core (
-  module Roller.Parse
-, module Roller.Types
-, module Options.Applicative
-, main
-) where
+module Roller.Core (main) where
 
 import Roller.Types
 import Roller.Parse
+import Roller.CLI
 
 import System.Environment (getArgs)
 import System.Random (randomRIO)
 import Control.Applicative hiding (Const)
-import Control.Monad
-import Options.Applicative hiding (Const)
+import Control.Monad (join, replicateM, replicateM_)
 
 rolls :: Int -> Int -> IO [Int]
 rolls n s = replicateM n . randomRIO $ (1,s)
@@ -23,8 +18,6 @@ roll de =
     Sum e1 e2 -> (++) <$> roll e1 <*> roll e2
     Const n   -> return [[n]]
     Die n s   -> return <$> n `rolls` s
-
-type CLI a = Bool -> Int -> [String] -> a
 
 rollEm :: CLI (IO ())
 rollEm verbose n args = maybe parseFail rollMany (parse input)
@@ -39,18 +32,4 @@ rollEm verbose n args = maybe parseFail rollMany (parse input)
 
 main :: IO ()
 main = join . withOpts $ rollEm
-
-withOpts :: CLI a -> IO a
-withOpts f = execParser . info (helper <*> handleOpts) $ infoMod
-  where
-  handleOpts =
-    f <$> switch ( long "verbose"
-                <> short 'v'
-                <> help "List out each roll")
-      <*> option ( long "nrolls"
-                <> value 1
-                <> short 'n'
-                <> help "Number of times to roll the expression")
-      <*> arguments1 str (metavar "EXPR.." <> help "Dice expressions")
-  infoMod = (fullDesc <> progDesc "Roll some dice")
 
